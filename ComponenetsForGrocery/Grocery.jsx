435708cb449d51046c6c6c9b066d16f4b9eefd96
Princek4693 <161598595+Princek4693@users.noreply.github.com>
2024-05-26 23:42:51 +0530

    Add files via upload

---
+++ ComponenetsForGrocery/Grocery.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Grocery(){
+
+    
+    return (
+        <div>
+        <h1>
+        Grocery List:
+        </h1>
+            </div>
+    )
+}
+
+export default Grocery;
